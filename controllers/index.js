2e8c78516ecf6f5afcf6eabd0995ec7231d60c06
CGalko <cash.galko12@ncf.edu>
2021-05-14 10:54:48 -0400

    More deployment issues fixed

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api/workout-routes.js');
-const homeRoutes = require('./home-routes.js');
+const apiRoutes = require('./api/workoutroutes.js');
+const homeRoutes = require('./homeroute.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
